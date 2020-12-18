3f912ce0e6a85ff1fb4a285b9a7d947ade222555
Jake Armendariz <jakearmendariz99@gmail.com>
2020-12-18 13:26:39 -0800

    more relevant endpoints

--- src/endpoints.py
+++ src/endpoints.py
@@ -4,11 +4,9 @@ from src.course_parser import CourseParser
 from ucsc_courses import app
 from src.config import ConfigObject
 
-@app.route('/api/v1.0/get_term_info', methods=['GET'])
+@app.route('/api/v1.0/terms', methods=['GET'])
 def get_all_params():
-
     results = CourseParser.get_term_info()
-
     return jsonify(results)
 
 @app.route('/api/v1.0/search', methods=['GET'])
@@ -28,6 +26,8 @@ def search():
     instr_name_op = request.args.get('instr_name_op', default='=', type=str)
     instructor = request.args.get('instructor', default='', type=str)
     reg_status = request.args.get('reg_status', default='O', type=str)
+    if reg_status == 'open': reg_status = 'O'
+    elif reg_status == 'closed': reg_status = 'C'
     subject = request.args.get('subject', default='', type=str)
     term = request.args.get('term', default=CourseParser.get_term_info()[0].get('term_id'), type=str)
     times = request.args.get('times', default='', type=str)
@@ -82,135 +82,55 @@ def search():
 
     return jsonify(return_payload)
 
-@staticmethod
-@app.route('/api/v1.0/open_courses', methods=['GET'])
-def open_courses():
-    ConfigObject.logger.log_msg('Running the Open Courses API','INFO')
-    default_payload = ConfigObject.default_payload
 
-    # You can derive this payload using the Inspect Element feature, navigate to the Networks
-    # tab, and then search for classes on https://pisa.ucsc.edu/class_search
-
-
-    custom_payload = {
-        "binds[:catalog_nbr_op]:": "=",
-        "binds[:crse_units_op]:": "=",
-        "binds[:instr_name_op]:": "=",
-        "binds[:reg_status]:": "O",
-        "binds[:term]:": CourseParser.get_term_info()[0].get('term_id')
-    }
-
-    # Combines two dictionaries together
-    # See: https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
-    open_courses_payload = { **default_payload,  **custom_payload }
-
-    results = CourseParser.parse_classes_page(open_courses_payload)
-
-    return jsonify(results)
-
-
-@app.route('/api/v1.0/open_courses/<int:num_of_results>', methods=['GET'])
-def open_courses_num(num_of_results):
+@app.route('/api/v1.0/courses', methods=['GET'])
+def courses():
     ConfigObject.logger.log_msg('Running the Open Courses API', 'INFO')
     default_payload = ConfigObject.default_payload
 
     # You can derive this payload using the Inspect Element feature, navigate to the Networks
     # tab, and then search for classes on https://pisa.ucsc.edu/class_search
-    custom_payload = {
-        "action": "update_segment",
-        "binds[:catalog_nbr_op]:": "=",
-        "binds[:crse_units_op]:": "=",
-        "binds[:instr_name_op]:": "=",
-        "binds[:reg_status]:": "O",
-        "binds[:term]:": 2210,
-        "rec_dur":num_of_results
-    }
-
-    # Combines two dictionaries together
-    # See: https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
-
-    open_courses_payload = {**default_payload, **custom_payload}
-
-    results = CourseParser.parse_classes_page(open_courses_payload)
-
-    return jsonify(results)
-
-@app.route('/api/v1.0/courses/<string:status>', methods=['GET'])
-def courses(status):
-    ConfigObject.logger.log_msg('Running the Open Courses API', 'INFO')
-    default_payload = ConfigObject.default_payload
-
-    # You can derive this payload using the Inspect Element feature, navigate to the Networks
-    # tab, and then search for classes on https://pisa.ucsc.edu/class_search
-    reg_status = "O"
-    if 'open' in status.lower():
-        reg_status = "O"
-    elif 'all' in status.lower():
-        reg_status = "all"
-    else:
-        return jsonify({
-            'msg': 'Your only choices in this endpoint is /open or /all, otherwise, just use /open_courses'
-        })
-
     print(CourseParser.get_term_info()[0])
     custom_payload = {
         "binds[:catalog_nbr_op]:": "=",
         "binds[:crse_units_op]:": "=",
         "binds[:instr_name_op]:": "=",
-        "binds[:reg_status]:": str(reg_status),
+        "binds[:term]:": CourseParser.get_term_info()[0].get('term_id'),
+        "rec_dur": request.args.get('count', 25),
+        "subject": request.args.get('subject', ''),
+        "reg_status": request.args.get('reg_status', 'all')
+    }
+    # Combines two dictionaries together
+    # See: https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
+    open_courses_payload = {**default_payload, **custom_payload}
+
+    results = CourseParser.parse_classes_page(open_courses_payload)
+
+    return jsonify(results)
+
+@app.route('/api/v1.0/course/<string:course>', methods=['GET'])
+def find_course(course):
+    print(CourseParser.get_term_info()[0])
+    nums = [str(i) for i in range(1, 11)]
+    subject_len = 3 if course[3] in nums else 4
+    custom_payload = {
+        "binds[:catalog_nbr]:": course[subject_len:],
+        "binds[:crse_units_op]:": "=",
+        "binds[:instr_name_op]:": "=",
+        "binds[:reg_status]:": 'all',
+        "binds[:subject]":course[:subject_len].upper(),
         "binds[:term]:": CourseParser.get_term_info()[0].get('term_id')
     }
 
     # Combines two dictionaries together
     # See: https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
-    open_courses_payload = {**default_payload, **custom_payload}
+    open_courses_payload = {**ConfigObject.default_payload, **custom_payload}
 
-    results = CourseParser.parse_classes_page(open_courses_payload)
-
-    return jsonify(results)
-
-@app.route('/api/v1.0/courses/<string:status>/<int:num_of_results>', methods=['GET'])
-def courses_num(status, num_of_results):
-    ConfigObject.logger.log_msg('Running the Open Courses API', 'INFO')
-    default_payload = ConfigObject.default_payload
-
-    # You can derive this payload using the Inspect Element feature, navigate to the Networks
-    # tab, and then search for classes on https://pisa.ucsc.edu/class_search
-    reg_status = "O"
-    if 'open' in status.lower():
-        reg_status = "O"
-    elif 'all' in status.lower():
-        reg_status = "all"
-    else:
-        return jsonify({
-            'msg': 'Your only choices in this endpoint is /open or /all, otherwise, just use /open_courses'
-        })
-    
-    bind_term = request.args.get('term', default=CourseParser.get_term_info()[0].get('term_id'), type=str)
-    custom_payload = {
-        "action": "update_segment",
-        "binds[:catalog_nbr_op]:": "=",
-        "binds[:crse_units_op]:": "=",
-        "binds[:instr_name_op]:": "=",
-        "binds[:reg_status]:": str(reg_status),
-        "binds[:term]:":bind_term,
-        "rec_dur": num_of_results
-    }
-
-    # Combines two dictionaries together
-    # See: https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
-    open_courses_payload = {**default_payload, **custom_payload}
-
-    # Parse resulting payload into the results object
     results = CourseParser.parse_classes_page(open_courses_payload)
 
     return jsonify(results)
 
 ### error handling
-
 @app.errorhandler(404)
 def not_found(error):
-    return   make_response(jsonify({'error': 'Not found'}), 404)
-
-def run(self, debug=True, port=5000):
-    self.app.run(port=port, debug=debug)
+    return make_response(jsonify({'error': 'Not found'}), 404)
